0128518a0be38c420ae286e84d882c950f48bffc
goodaytm <matsush@gmail.com>
2016-05-02 10:32:25 +0000

    kadai_col_tochu

--- db/migrate/20160502005311_add_age_to_msessages.rb
+++
@@ -1,5 +0,0 @@
-class AddAgeToMsessages < ActiveRecord::Migration
-  def change
-    add_column :msessages, :age, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427214910) do
+ActiveRecord::Schema.define(version: 20160502005311) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
-    t.string   "age"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
